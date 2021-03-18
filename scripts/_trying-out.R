25c08a5f3c5ab06fd13fb47d35ce26f983d9fa4e
sebastianplnr <sebastian.ploner@gmail.com>
2021-03-18 14:53:31 +0100

    New improved vis

--- scripts/_trying-out.R
+++ scripts/_trying-out.R
@@ -20,17 +20,10 @@ data$country = as.factor(data$country)
 
 # reducing data to essential variables such that it is smaller and therefore easier to handle
 reduced_weighted_data = data %>%
-  select(country, date, year, week, rank, valence, danceability, energy, tempo) %>%
-  mutate(reversed_rank = 201 - rank,
-         weighted_valence = valence * reversed_rank,
-         weighted_danceability = danceability * reversed_rank,
-         weighted_energy = energy * reversed_rank,
-         weighted_tempo = tempo * reversed_rank) %>% 
+  select(country, date, year, week, rank, valence) %>%
+  mutate(reversed_rank = 201 - rank, weighted_valence = valence * reversed_rank) %>% 
   group_by(country, year, week) %>% 
-  summarise(avg_weighted_valence = mean(weighted_valence),
-            avg_weighted_danceability = mean(weighted_danceability),
-            avg_weighted_energy = mean(weighted_energy),
-            avg_weighted_tempo = mean(weighted_tempo))
+  summarise(avg_weighted_valence = mean(weighted_valence))
 
 
 # train model to predict 2020 valence scores which will serve as baseline to compare the actual 2020 data against
@@ -48,38 +41,13 @@ valence_pred = predict(lm1, newdata = test_data)
 # merged actual and predicted data sets 
 combined_data = cbind(test_data, data.frame(valence_pred))
 
+
 # creating a year/week variable for easier calculation 
 combined_data$year_week = make_date(year = combined_data$year) + weeks(combined_data$week)
 
 combined_data = combined_data %>%
   mutate(difference = round((valence_pred - avg_weighted_valence), digits = 2),
          proportion_of_deviation = round((difference/valence_pred), digits = 2))
-
-
-
-valence_heatmap = combined_data %>%
-  
-  mutate(continent = as.factor(countrycode(country, "country.name", "continent", warn = FALSE, nomatch = NULL))) %>% 
-  filter(continent == "Europe", country != "Cyprus") %>% 
-  
-  ggplot(aes(year_week, country, fill = proportion_of_deviation*100)) + geom_tile() +
-  
-  scale_fill_gradientn(colours = c("#971D2B", "#FDFDC3", "#2B653C"),
-                       values = c(0, 0.5, 1),
-                       limits = c(-27, 27)) +
-  theme_classic() +
-  theme(plot.title = element_text(size = 18, face = "bold")) +
-  
-  labs(title = "Happiness Difference by Country and Week in 2020 compared to previous Years",
-       subtitle = "Average difference between 2020 predicted and actual happiness scores",
-       caption = "")
-
-valence_heatmap
-
-valence_heatmap_html = ggplotly(valence_heatmap)
-valence_heatmap_html
-
-
 
 
 valence_line_plot = combined_data %>%
@@ -91,9 +59,7 @@ valence_line_plot = combined_data %>%
   theme(panel.grid.major = element_line(size = 0.1, colour = "gray"),
         plot.title = element_text(size = 18, face = "bold")) +
   
-  scale_x_date(name = "",
-               date_breaks = "2 month",
-               date_labels = "%b %Y") +
+  scale_x_date(name = "", date_breaks = "2 month", date_labels = "%b %Y") +
   scale_y_continuous(name = "Deviation from expected valence",
                      labels = scales::percent_format(accuracy = 1L),
                      limits = c(-0.25, 0.25)) +
@@ -105,14 +71,13 @@ valence_line_plot = combined_data %>%
 ggsave("figures/valence.png", test2, width = 10, height = 6)
 
 
-
-
+# smoothing the proportion of deviation
 models = combined_data %>%
   tidyr::nest(-country) %>%
   dplyr::mutate(
     # Perform loess calculation on each country
     m = purrr::map(data, loess,
-                   formula = proportion_of_deviation ~ as.numeric(year_week), span = .5),
+                   formula = proportion_of_deviation ~ as.numeric(year_week), span = 0.2),
     # Retrieve the fitted values from each model
     fitted = purrr::map(m, `[[`, "fitted")
   )
@@ -122,56 +87,41 @@ results = models %>%
   dplyr::select(-m) %>%
   tidyr::unnest()
 
-# Plot with loess line for each group
+
+# heat map for smoothed data
+# valence_heatmap = 
 results %>%
-  filter(country != "Cyprus") %>%
-  ggplot(aes(x = year_week, y = proportion_of_deviation, group = country, colour = country)) +
-  geom_line(aes(y = fitted))
+  filter(country != "Cyprus", year_week < "2021-02-05") %>%
+  ggplot(aes(year_week, country, fill = fitted)) +
+  geom_tile(color="white") +
+  
+  scale_fill_gradientn(colours = c("#971D2B", "#FDFDC3", "#2B653C"),
+                       values = c(0, 0.5, 1),
+                       limits = c(-0.26, 0.26),
+                       labels = scales::percent_format(accuracy = 1L),
+                       name = "Deviation from\nexpected valence") +
+  
+  scale_x_date(name = "",
+               date_breaks = "2 month",
+               date_labels = "%b %Y",
+               expand = c(0,0),
+               sec.axis = dup_axis()) +
+  
+  labs(title = "Happiness Difference by Country and Week in 2020 compared to previous Years",
+       subtitle = "Average difference between 2020 predicted and actual happiness scores",
+       caption = "", x = "", y = "") +
+  
+  theme_classic() +
+  
+  theme(plot.title = element_text(size = 18, face = "bold"),
+        axis.line.y = element_blank(),
+        axis.ticks.y = element_blank())
 
+valence_heatmap
+ggsave("figures/valence_heatmap.png", valence_heatmap, width = 15, height = 13)
 
-results %>% ggplot(aes(year_week2, country, fill = fitted)) + geom_tile() +
-  scale_fill_gradientn(colours = c("#971D2B", "#FDFDC3", "#2B653C"), values = c(0, 0.5, 1), limits = c(-.25, 0.25))
-
-
-
-
-
-
-
-
-
-
-
-
-x = loess(valence_pred ~ as.numeric(year_week2), degree = 0, span = 0.1, data = data2)$fitted
-
-ggplot(data2, aes(year_week2, x)) + geom_line()
-
-
-
-test3 = data2 %>% 
-  mutate(difference = valence_pred - avg_weighted_valence,
-         deviation_in_percent = round((difference/valence_pred), digits = 2))
-
-
-for (country in test3$country) {
-  t1 = test3 %>% filter(country == as.character(country)) %>% 
-    (loess(deviation_in_percent ~ as.numeric(year_week2), degree = 0, span = 0.1, data = test3)$fitted)
-  answer = append(answer, t1)
-}
-
-%>% 
-  (loess(deviation_in_percent ~ as.numeric(year_week2), degree = 0, span = 0.1, data = test3)$fitted)
-answer = append(answer, t1)
-
-
-
-
-
-
-
-
-lapply(as.factor(test3$country), loess, test3$deviation_in_percent ~ as.numeric(test2$year_week2), degree = 0, span = 0.1)
+valence_heatmap_html = ggplotly(valence_heatmap)
+valence_heatmap_html
 
 
 
@@ -180,4 +130,5 @@ x = loess(deviation_in_percent ~ as.numeric(year_week2), degree = 0, span = 0.1,
 
 test3 %>% ggplot(aes(year_week2, country, fill = x)) + geom_tile() +
   scale_fill_gradientn(colours = c("#971D2B", "#FDFDC3", "#2B653C"), values = c(0, 0.5, 1), limits = c(-0.16, 0.16))
+  
 
